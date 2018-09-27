ed1916686cf1f617e170732f140f1a22c75bc1ce
Diego Garcia <wft2000@gmail.com>
2018-09-27 20:12:00 +0200

    feature grid connected to backend

--- src/components/BacklogManagement.vue
+++ src/components/BacklogManagement.vue
@@ -1,109 +1,74 @@
 <template>
     <div>
         <v-toolbar flat color="white">
-        <v-toolbar-title>{{ title }}</v-toolbar-title>
-        <v-divider
-            class="mx-2"
-            inset
-            vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+            <v-divider
+                class="mx-2"
+                inset
+                vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
         </v-toolbar>
                 
         <v-data-table
             :headers="headers"
             :items="rows"
-            hide-actions
+            :search="search"
+            v-model="selected"
+            item-key="code"
+            select-all
+            :rows-per-page-items="rowsPerPageItems"    
             class="elevation-1"
         >
             <template slot="items" slot-scope="props">
-            <td class="text-xs-left">{{ props.item.code }}</td>
-            <td class="text-xs-left">{{ props.item.name }}</td>
-            <td class="text-xs-right">{{ props.item.estimation }}</td>
-            <td class="text-xs-right">{{ props.item.completed }}</td>
-            <td class="text-xs-left">{{ props.item.status }}</td>
+                <td>
+                    <v-checkbox
+                    v-model="props.selected"
+                    primary
+                    hide-details
+                    ></v-checkbox>
+                </td>
+                <td class="text-xs-left">{{ props.item.code }}</td>
+                <td class="text-xs-left">{{ props.item.name }}</td>
+                <td class="text-xs-right">{{ props.item.estimation }}</td>
+                <td class="text-xs-right">{{ props.item.pctCompleted }}</td>
+                <td class="text-xs-left">{{ props.item.status.description }}</td>
             </template>
         </v-data-table>
     </div>
 </template>
 <script>
+import axios from '../../node_modules/axios/dist/axios.min'
+import config from '../config/Configuration'
+
 export default {
     name: 'BacklogManagement',
     data () {
       return {
         title: 'Release Backlog',
+        search: '',
+        selected: [],        
+        rowsPerPageItems: config.rowsPerPageItems,
         headers: [
           {
             text: 'Feature',
             align: 'left',
             sortable: true,
             value: 'feature',
-            width: '150px'
+            width: '10%'
           },
-          { text: 'Name', align: 'left', sortable: true, value: 'name', width: '650px' },
-          { text: 'Estimation', align: 'right', value: 'estimation', width: '80px' },
-          { text: '% Completed', align: 'right', value: 'completed', width: '80px' },
-          { text: 'Status', value: 'status', width: '250px' }
+          { text: 'Name', align: 'left', sortable: true, value: 'name', width: '50%' },
+          { text: 'Estimation', align: 'right', value: 'estimation', width: '10%' },
+          { text: '% Completed', align: 'right', value: 'completed', width: '10%' },
+          { text: 'Status', value: 'status', width: '15%' }
         ],
-        rows: [
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         },
-         {
-             code: '1232456',
-             name: 'autheticated acces and role verification',
-             estimation: 120,
-             completed: 50,
-             status: 'in progress'
-         }                                     
-        ]
-      }
-    }    
+        rows: []
+      };
+    },
+    mounted () {
+        axios
+            .get(config.apiBaseUrl + 'features/')
+            .then(response => (this.rows = response.data._embedded.features))
+    } 
 }
 </script>
---
+++ src/config/Configuration.js
@@ -0,0 +1,4 @@
+export default {
+    apiBaseUrl: 'http://localhost:8081/',
+    rowsPerPageItems: [10, 20, 50, {text: 'ALL', value: -1}]
+}
